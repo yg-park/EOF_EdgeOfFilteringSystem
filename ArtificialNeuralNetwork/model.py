95b098d3a5a2901ebdb7cec98f11ee9f8e9840b9
kenGwon <kgh9959@gmail.com>
2023-12-22 08:42:01 +0900

    클라이언트 제어모듈 추가

--- ArtificialNeuralNetwork/model.py
+++ ArtificialNeuralNetwork/model.py
@@ -52,8 +52,53 @@ class bottle_classifier:
 PATH = './eof_bottle_classification_model.pth'
 eof_model = bottle_classifier(PATH)
 
+"""
 test_img = cv2.imread("test_input4.jpg")
 result = eof_model.classify(test_img)
-
 print(f"추론결과: {result}")
+test_img = cv2.imread("test_input3.jpg")
+result = eof_model.classify(test_img)
+print(f"추론결과: {result}")
+test_img = cv2.imread("test_input2.jpg")
+result = eof_model.classify(test_img)
+print(f"추론결과: {result}")
+test_img = cv2.imread("test_input1.jpg")
+result = eof_model.classify(test_img)
+print(f"추론결과: {result}")
+"""
 
+
+import cv2
+
+# 웹캠 열기 (0은 기본 웹캠을 의미)
+cap = cv2.VideoCapture(0)
+
+# 웹캠이 정상적으로 열렸는지 확인
+if not cap.isOpened():
+    print("Error: Could not open webcam.")
+    exit()
+
+# 무한 루프로 프레임 읽기
+while True:
+    # 프레임 읽기
+    ret, frame = cap.read()
+
+    # 프레임 읽기에 문제가 있으면 루프 종료
+    if not ret:
+        print("Error: Could not read frame.")
+        break
+
+    result = eof_model.classify(frame)
+    print(f"추론결과: {result}")
+    
+
+    # 프레임을 화면에 표시
+    cv2.imshow("Webcam", frame)
+    
+    # 'q' 키를 누르면 루프 종료
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# 사용한 자원 해제
+cap.release()
+cv2.destroyAllWindows()
