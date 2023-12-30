07d4831e3ec992f92937892d37beadf4014bc950
DohyeonP <ehgus922@gmail.com>
2023-12-30 17:31:45 +0900

    서버 통신 클래스와 추론 클래스의 틀 제작

---
+++ EOF_TRASH_SERVER/Comm/string_comm.py
@@ -0,0 +1,15 @@
+from communication import Communication
+
+class String_comm(Communication):
+    def __init__(self):
+        # TODO: 문자열 통신과 관련된 포트 설정
+        # TODO: 소켓 생성 및 초기화
+    
+    def receive(self):
+        # TODO: 문자열을 수신
+        
+        # return 문자열
+    
+    def send(self, ip_address, port):
+        # TODO: 문자열을 송신
+        
---
+++ EOF_TRASH_SERVER/Comm/voice_comm.py
@@ -0,0 +1,13 @@
+from communication import Communication
+
+class Voice_comm(Communication):
+    def __init__(self):
+        # TODO: 음성 메세지 통신과 관련된 포트 설정
+        # TODO: 소켓 생성 및 초기화
+    
+    def receive(self):
+        # TODO: 음성 메세지 수신 및 저장
+        
+        # return voice_file_path
+        
+        
