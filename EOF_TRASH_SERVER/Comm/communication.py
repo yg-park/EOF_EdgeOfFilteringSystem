3cc007b0324f3fcddd6a99eb1f44deeaf11f1a1a
woochang0110 <woochang0110@gmail.com>
2024-01-03 11:17:53 +0900

    실시간 영상전송 프레임 문제 해결 + 코드 리팩토링

--- EOF_TRASH_SERVER/Comm/communication.py
+++ EOF_TRASH_SERVER/Comm/communication.py
@@ -1,7 +1,21 @@
+"""
+d
+"""
 from abc import ABC, abstractmethod
 
+
 class Communication(ABC):
+    """
+    d
+    """
+    def __init__(self):
+        self.ip_address = "10.10.15.58"
+        self.img_port = 5555
+        self.str_port = 6666
+        self.wav_port = 7777
+        self.tmp_port = 8888
     
     @abstractmethod
     def receive(self):
+        """d"""
         pass
--- EOF_TRASH_SERVER/main.py
+++ EOF_TRASH_SERVER/main.py
@@ -1,12 +1,22 @@
+"""
+프로그램 엔트리 포인트 모듈입니다.
+"""
 import sys
 from PyQt5.QtWidgets import QApplication
-from GUI import *
+from PyQt_framework.GUI import MainGUI
 
+IP_ADDRESS = "10.10.15.58"
+IMG_PORT = 5555
+STR_PORT = 6666
+WAV_PORT = 7777
+TMP_PORT = 8888
 
 if __name__ == "__main__":
+    port = {"IMG_PORT": IMG_PORT, "STR_PORT": STR_PORT,
+            "WAV_PORT": WAV_PORT, "TMP_PORT": TMP_PORT}
 
     app = QApplication(sys.argv)
-    window = MainGUI()
+    window = MainGUI(IP_ADDRESS, port)
     window.setGeometry(100, 100, 1200, 800)
     window.show()
     sys.exit(app.exec_())
