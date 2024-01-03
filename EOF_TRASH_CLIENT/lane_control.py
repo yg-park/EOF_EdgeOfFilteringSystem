c5342bea8d8bc394dc1aac3728cc099262bfaa9c
woochang0110 <woochang0110@gmail.com>
2024-01-03 18:04:02 +0900

    클라이언트측 문자열 수신 및 수신 데이터에 따른 HW 제어 추가

--- EOF_TRASH_CLIENT/lane_control.py
+++ EOF_TRASH_CLIENT/lane_control.py
@@ -8,6 +8,7 @@ from GPIO_HW_control.rc_servo_motor import RCServoMotor
 from GPIO_HW_control.servo_motor import ServoMotor
 from Comm.communication import ClientCommunication
 from Comm.send_audio import AudioCommunication
+from Comm.rcv_string import StringComm
 from audio.voice_record import AudioRecorder
 
 class LaneController:
@@ -20,6 +21,7 @@ class LaneController:
         """
         self.rc_servo_motor = RCServoMotor(pin["RC_SERVO_1"], pin["RC_SERVO_2"])
         self.servo_motor = ServoMotor(pin["SERVO"])
+        self.rcv_str_comm = StringComm()
         self.camera = cv2.VideoCapture(0)
         self.ip_address = ip_address
         self.port = port
@@ -35,9 +37,10 @@ class LaneController:
         self.comm = ClientCommunication(self.ip_address, self.port["PORT_IMAGE"])
         self.webcam_thread = threading.Thread(target=self.send_webcam_thread)
         self.voice_thread = threading.Thread(target=self.send_voice_thread)
-        self.HW_thread = threading.Thread(target=self.rcv_HW_comm)
+        self.HW_thread = threading.Thread(target=self.HW_control)
         self.servo_thread = threading.Thread(target=self.servo_motor.kick)
-    
+        self.listen_str_thread = threading.Thread(target=self.rcv_str_comm.receive)
+
     def send_voice_thread(self):
         recorder = AudioRecorder()
         recorder.start_recording()
@@ -55,30 +58,30 @@ class LaneController:
             self.comm.send_frame(frame)
             time.sleep(1/60)        
             
-    def rcv_HW_comm(self):
+    def HW_control(self):
         """
         hw관련 서버로부터 통신 받는 쓰레드 함수
         서보모터(1)/rc서보모터 on(2) off(3)
         """
-        msg = '2'
         while True:
-            # TODO: TCPIP 통신 servo 제어 문자가 오면 ->HW제어
-            # if 수신받은 메시지 == ' ':
-            if msg == '1':
+            if self.rcv_str_comm.msg == 1:
                 self.servo_thread.start()
-            elif msg == '2':
+                self.rcv_str_comm.msg = 0
+            elif self.rcv_str_comm.msg == 2:
                 self.rc_servo_motor.start()
-            elif msg == '3':
+                self.rcv_str_comm.msg = 0
+            elif self.rcv_str_comm.msg == 3:
                 self.rc_servo_motor.stop()
-    
-    
-
+                self.rcv_str_comm.msg = 0
+ 
     def execute(self):
         try:
             self.webcam_thread.start()
             self.HW_thread.start()
             self.voice_thread.start()
+            self.listen_str_thread.start()
         finally:
             self.webcam_thread.join()
             self.HW_thread.join()
             self.voice_thread.join()
+            self.listen_str_thread.join()
