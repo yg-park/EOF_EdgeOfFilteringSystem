50f504cb6bd21e39578b5bdb46b3293927277c19
ehgus922@gmail.com <ehgus922@gmail.com>
2024-01-13 19:26:43 +0900

    whisper 성능 향상 및 GPU로 추론하도록 변경, LLAMA2 모델을 API로 사용

--- EOF_TRASH_SERVER/utils/Inference/voice_inferencer.py
+++ EOF_TRASH_SERVER/utils/Inference/voice_inferencer.py
@@ -2,7 +2,10 @@
 whisper와 llama2 recipes를 바탕으로 오디오 파일에 대한 추론 작업을 지원하는 모듈입니다.
 """
 import os
+import configparser
+
 import whisper
+from googletrans import translate
 from langchain_community.llms import Replicate
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
@@ -10,24 +13,26 @@ from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain.chains import ConversationalRetrievalChain
 
-LLAMA2_7B_CHAT = 'meta/llama-2-7b-chat:13c3cdee13ee059ab779f0291d29054dab00a47dad8261375654de5540165fb0'
-LLAMA2_7B = "meta/llama-2-7b:77dde5d6c56598691b9008f7d123a18d98f40e4b4978f8a72215ebfc2553ddd8"
-REPLICATE_API_TOKEN = 'r8_FC0oSTN78VxAqc8cWbMUSUhJADRPvtG2zFyNu'
-
 
 class VoiceInferencer:
     """Speech To Text, Text Generation을 지원하는 클래스 입니다."""
+
+    config = configparser.ConfigParser()
+    config.read("resources/api_tokens.ini")
+
     def __init__(self):
-        self.stt_model = whisper.load_model("base")
+        self.stt_model = whisper.load_model("medium")
         self.lmm_chain_model = self._init_llama2_recipes()
 
     def _init_llama2_recipes(self):
         # API
-        os.environ["REPLICATE_API_TOKEN"] = REPLICATE_API_TOKEN
+        os.environ["REPLICATE_API_TOKEN"] = \
+            self.config["REPLICATE"]["API_TOKEN"]
+
         # 모델 준비
         llama_model = Replicate(
-            model=LLAMA2_7B_CHAT,
-            model_kwargs={"temperature": 0.95, "top_p": 0.95, "max_new_tokens": 500}
+            model=self.config["LLAMA2"]["7B"],
+            model_kwargs={"temperature": 0.4, "top_p": 0.85, "max_new_tokens": 50}
         )
         # PDF 파일 준비
         loader = PyPDFLoader("resources/User_Manual.pdf")
@@ -38,7 +43,7 @@ class VoiceInferencer:
         # 자연어 처리에서 텍스트의 특징을 추출하기 위해 벡터화를 해야한다.
         # 허깅페이스에서 제공하는 embedding 모델을 사용
         model_name = "sentence-transformers/all-mpnet-base-v2"
-        model_kwargs = {"device": "cpu"}
+        model_kwargs = {"device": "cuda"}
         embeddings = HuggingFaceEmbeddings(model_name=model_name, model_kwargs=model_kwargs)
         # FAISS는 고차원 벡터 데이터의 유사성 검색을 빠르게 수행하는 도구
         vectorstore = FAISS.from_documents(all_splits, embeddings)
@@ -49,8 +54,15 @@ class VoiceInferencer:
 
     def get_stt(self, audio_file_path):
         """whisper를 통한 speech to text로 생성된 문자열을 리턴합니다."""
-        stt = self.stt_model.transcribe(audio_file_path, language="ko", fp16=False)
-        return stt["text"].strip()
+        audio = whisper.load_audio(audio_file_path)
+        audio = whisper.pad_or_trim(audio)
+
+        mel = whisper.log_mel_spectrogram(audio).to('cuda')
+
+        options = whisper.DecodingOptions(language="en")
+        result = whisper.decode(self.stt_model, mel, options)
+
+        return result.text.lower()
 
     def get_llama2_answer(self, text):
         """PDF 사용설명서 기반의 llama2 recipe로 사용자의 질문에 대한 응답을 생성합니다."""
@@ -59,5 +71,5 @@ class VoiceInferencer:
         response = result['answer']
         response = response[:response.find('\n')]
 
-        print(f"Answer: {response}")
+        response = translate(response, "ko")
         return response
